4c612b22a8de35f1a916c064f59168abf61cb489
karan parashar <91617822+karan006@users.noreply.github.com>
2024-01-21 21:44:25 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/**
+ * product
+ */
+public class product {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int product=a * b;
+            System.out.println(product);
+        }
+    }
+
+    
+}
+    
+    
