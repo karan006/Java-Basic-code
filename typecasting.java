276ab9687e824e497c32688559bde00105b04f8a
karan parashar <91617822+karan006@users.noreply.github.com>
2024-01-21 21:42:51 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,7 @@
+public class typecasting {
+    public static void main(String args[]){
+        float number = 99.999f;
+        int number2 =(int)number;
+        System.out.println(number2);
+    }
+}
