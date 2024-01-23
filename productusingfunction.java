b586805bbcdeb92cea55582d94fd95559f4af9b7
karan parashar <91617822+karan006@users.noreply.github.com>
2024-01-23 23:33:02 +0530

    Add files via upload

---
+++ productusingfunction.java
@@ -0,0 +1,11 @@
+public class productusingfunction {
+    public static int multiply(int a, int b) {
+        int product=a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int prod= multiply(3,4);
+        System.out.println(prod);
+        
+    }
+}
